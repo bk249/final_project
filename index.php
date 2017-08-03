c45c087f0a75a96cfff5658a36c88ef411f0a05a
Bee Kim <bk249@njit.edu>
2017-08-03 16:27:22 -0400

    first files added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>To Do List</title>
+
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+		<link rel="stylesheet" href="css/main.css">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	<body>
+		<div class="list">
+			<h1 class="header">To Do.</h1>
+
+			<ul class="items">
+				<li><span class="item">Pick up shopping</span></li>
+				<li><span class="item done">Learn php</span></li>
+			</ul>
+
+			<form class="item-add" action= "add.php" method="post">
+				<input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
+				<input type="submit" value="Add" class="submit">
+	</body>
+</html>
